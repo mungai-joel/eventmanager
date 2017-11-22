d74a5f8222541c3bbd936f6d68ca141506469fae
mungaijoel <mungaijoel123@gmail.com>
2017-11-22 17:47:35 +0300

    Made changes to the Flask Structure to enable it to run

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+from app import app
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.secret_key = "secretkey4321"
+    app.run(debug=True)
